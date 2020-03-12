bb94e644c6c69479fce3b4e2bfdbc7de2175fb2b
Paul Gilchrist <paul.gilchrist@outlook.com>
2020-03-12 14:38:01 -0400

    Updates

---
+++ precache-manifest.b900196fa7dc76615033851d6e6fcb95.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce8fc400a878b1bd7d54d7a4fdfd972",
+    "url": "/ballistics-react/index.html"
+  },
+  {
+    "revision": "c14be043eb57408b1974",
+    "url": "/ballistics-react/static/css/2.54792d1d.chunk.css"
+  },
+  {
+    "revision": "28adcfdf0552f37e6f7b",
+    "url": "/ballistics-react/static/css/main.536b7319.chunk.css"
+  },
+  {
+    "revision": "c14be043eb57408b1974",
+    "url": "/ballistics-react/static/js/2.973ec829.chunk.js"
+  },
+  {
+    "revision": "5298b0565bb315b9314a8baf33940c9d",
+    "url": "/ballistics-react/static/js/2.973ec829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28adcfdf0552f37e6f7b",
+    "url": "/ballistics-react/static/js/main.64a7a4cd.chunk.js"
+  },
+  {
+    "revision": "83cc9a262ca3f7961963",
+    "url": "/ballistics-react/static/js/runtime-main.a13a94ef.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ballistics-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ballistics-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ballistics-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ballistics-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ballistics-react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
