1a803ceb684846fa0c8279d1d1e2759cab41b2c4
Paul Gilchrist <paul.gilchrist@outlook.com>
2020-09-04 10:04:29 -0400

    Updates

---
+++ precache-manifest.bf37a0e82742b2a3af350347327ee1a9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2bacdb66835aabd3e1a85aafec134dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c4a677be385205c36fe",
+    "url": "/static/css/2.9ac66d73.chunk.css"
+  },
+  {
+    "revision": "dd8dbbf4760bc56c25b4",
+    "url": "/static/css/main.15f6a0b0.chunk.css"
+  },
+  {
+    "revision": "6c4a677be385205c36fe",
+    "url": "/static/js/2.06c5d2fe.chunk.js"
+  },
+  {
+    "revision": "eee9feb83ad2afb562636af90f69183d",
+    "url": "/static/js/2.06c5d2fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd8dbbf4760bc56c25b4",
+    "url": "/static/js/main.97143215.chunk.js"
+  },
+  {
+    "revision": "57f21bfb84e2259fa983",
+    "url": "/static/js/runtime-main.94f63e1f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ballistics-react/precache-manifest.ec695861ac489d86f135bcf6461fb9c0.js"
+  "/precache-manifest.bf37a0e82742b2a3af350347327ee1a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ballistics-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
